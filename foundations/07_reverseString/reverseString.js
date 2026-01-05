664179c204d1f7f9586800eebc4762c21c76c23c
Freddy <frederickyamah@gmail.com>
2026-01-04 18:54:02 -0600

    reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const arr = word.split('');
+    arr.reverse();
+    const reversed = arr.join('');
+    return reversed;
 };
 
 // Do not edit below this line
