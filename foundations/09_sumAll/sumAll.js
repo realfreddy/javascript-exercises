6435f35939f5edf012cb59159c1d785c5dc03844
Freddy <frederickyamah@gmail.com>
2026-01-18 10:59:01 -0600

    fixed sum all

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    let calc = 0;
+    if (num1 < num2){
+    for (let i = num1; i <= num2; i++){
+        calc += i
+    }
+} else {
+    for (let i = num1; i >= num2; i--){
+        calc += i
+    }
 
+}
+    return calc
 };
 
 // Do not edit below this line
