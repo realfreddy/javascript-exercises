278a98a1aa610da7256d81f4a644fdaf3fa4d578
Freddy <frederickyamah@gmail.com>
2026-01-24 19:26:27 -0600

    did math functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,x) => acc + x , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc,x) => acc * x , 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1;
+  for (let i = x; i > 0; i--) {
+    product *= i;
+  }
+  return product; 
 };
 
 // Do not edit below this line
